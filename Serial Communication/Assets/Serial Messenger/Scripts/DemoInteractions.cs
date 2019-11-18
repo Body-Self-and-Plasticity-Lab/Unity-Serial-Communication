2588afcb30b7ca69b8375e76ee5fa2b456348af6
marteroel <marteroel@users.noreply.github.com>
2019-11-18 19:05:48 +0100

    controls technorama wall

--- Serial Communication/Assets/Serial Messenger/Scripts/DemoInteractions.cs
+++ Serial Communication/Assets/Serial Messenger/Scripts/DemoInteractions.cs
@@ -12,17 +12,22 @@ public class DemoInteractions : MonoBehaviour {
 	public string messageToSend;//message to be sent to serial device
 
 
-	// Use this for initialization
-	void Start () {
-		serialController = GetComponent<SerialControl> ();	//loads the SerialControl script from this gameObject into serialController.
-	}
+    // Use this for initialization
+    void Start() {
+        serialController = GetComponent<SerialControl>();	//loads the SerialControl script from this gameObject into serialController.
+        serialController.WriteToPort("sys_on" + "\r" + "\n");
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetKeyDown("space")){
-			serialController.WriteToPort(messageToSend); //calls the WriteToPort function and sends a string to it.
+		if(Input.GetKeyDown("1")){
+			serialController.WriteToPort("wall_on" + "\r" + "\n"); //calls the WriteToPort function and sends a string to it.
 		}
+        if (Input.GetKeyDown("2"))
+        {
+            serialController.WriteToPort("wall_off" + "\r" + "\n"); 
+        }
 
-	}
+    }
 }
